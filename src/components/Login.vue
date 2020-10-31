1b4a0dcfed9dac6c0a1d842f27202524fbbd02a9
Gary Poon Tip <gpoontip@fieldsafesolutions.com>
2020-10-31 15:03:57 -0600

    add eslint and prettier rules

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,17 +1,21 @@
 <template>
-<div class="p-d-flex p-jc-center">
-<form>
-    <div class="p-fluid">
+  <div class="p-d-flex p-jc-center">
+    <form>
+      <div class="p-fluid">
         <div class="p-field">
-        <InputText type="text" v-model="email" placeholder="Email" />
+          <InputText type="text" v-model="email" placeholder="Email" />
         </div>
         <div class="p-field">
-        <InputText type="password" v-model="password" placeholder="Password" />
+          <InputText
+            type="password"
+            v-model="password"
+            placeholder="Password"
+          />
         </div>
-    </div>
-    <Button label="Submit" @click="login" />
-</form>
-</div>
+      </div>
+      <Button label="Submit" @click="login" />
+    </form>
+  </div>
 </template>
 
 <script>
@@ -21,18 +25,17 @@ import InputText from 'primevue/inputtext';
 export default {
   name: 'HelloWorld',
   components: {
-    Button, InputText
+    Button,
+    InputText,
   },
-  data(){
+  data() {
     return {
-      email:null,
-      password:null
-    }
+      email: null,
+      password: null,
+    };
   },
-  methods:{
-      login(){
-
-      }
-  }
-}
+  methods: {
+    login() {},
+  },
+};
 </script>
