13e96e6e1a892f1cec82ad5907e686844cf065da
Gary Poon Tip <gpoontip@fieldsafesolutions.com>
2020-11-21 13:43:18 -0700

    Add date filter and delete button

--- src/components/BookingsTable.vue
+++ src/components/BookingsTable.vue
@@ -1,159 +1,162 @@
 <template>
-  <div>
-    <DataTable
-      :value="data"
-      :paginator="true"
-      :rows="5"
-      :loading="loading"
-      :filters="filters"
-      removableSort
-      class="p-datatable-responsive p-input-filled"
-      ref="dt"
+  <DataTable
+    :value="dataFormatted"
+    :paginator="true"
+    :rows="5"
+    :loading="loading"
+    :filters="filters"
+    removableSort
+    class="p-datatable-responsive p-input-filled"
+    ref="dt"
+  >
+    <template #header>
+      <div class="table-header">
+        <span class="p-input-icon-left">
+          <i class="pi pi-search" />
+          <InputText v-model="filters['global']" placeholder="Global Search" />
+        </span>
+        <Button
+          icon="pi pi-external-link"
+          label="Export"
+          @click="exportCSV($event)"
+        />
+      </div>
+    </template>
+    <template #empty> No customers found. </template>
+    <template #loading> Loading... Please wait. </template>
+    <Column
+      :sortable="true"
+      field="name"
+      header="Name of Production"
+      filterMatchMode="contains"
     >
-      <template #header>
-        <div class="table-header">
-          <span class="p-input-icon-left">
-            <i class="pi pi-search" />
-            <InputText
-              v-model="filters['global']"
-              placeholder="Global Search"
-            />
-          </span>
-          <Button
-            icon="pi pi-external-link"
-            label="Export"
-            @click="exportCSV($event)"
-          />
-        </div>
+      <template #body="slotProps">
+        <span class="p-column-title">Name of Production</span>
+        <span>{{ slotProps.data.name }}</span>
       </template>
-      <template #empty> No customers found. </template>
-      <template #loading> Loading... Please wait. </template>
-      <Column
-        :sortable="true"
-        field="name"
-        header="Name of Production"
-        filterMatchMode="contains"
-      >
-        <template #body="slotProps">
-          <span class="p-column-title">Name of Production</span>
-          <span>{{ slotProps.data.name }}</span>
-        </template>
-        <template #filter>
-          <InputText
-            type="text"
-            v-model="filters['name']"
-            class="p-column-filter"
-            placeholder="Name"
-          />
-        </template>
-      </Column>
-      <Column
-        :sortable="true"
-        field="date"
-        header="Date/Time"
-        filterMatchMode="custom"
-        :filterFunction="filterDate"
-      >
-        <template #body="slotProps">
-          <span class="p-column-title">Date/Time</span>
-          {{ slotProps.data.date }} {{ slotProps.data.time }}
-        </template>
-        <template #filter>
-          <Calendar
-            v-model="filters['date']"
-            dateFormat="yy-mm-dd"
-            class="p-column-filter"
-            placeholder="Date"
-          />
-        </template>
-      </Column>
-      <Column
-        :sortable="true"
-        field="amount"
-        header="Number of Testees"
-        filterMatchMode="contains"
-      >
-        <template #body="slotProps">
-          <span class="p-column-title">Number of Testees</span>
-          <span>{{ parseInt(slotProps.data.amount) }}</span>
-        </template>
-        <template #filter>
-          <InputText
-            type="text"
-            v-model="filters['amount']"
-            class="p-column-filter"
-            placeholder="Testees"
-          />
-        </template>
-      </Column>
-      <Column
-        :sortable="true"
-        field="location.name"
-        header="Location"
-        filterMatchMode="contains"
-      >
-        <template #body="slotProps">
-          <span class="p-column-title">Location</span>
-          <span>{{ slotProps.data.location.name }}</span>
-        </template>
-        <template #filter>
-          <InputText
-            type="text"
-            v-model="filters['location.name']"
-            class="p-column-filter"
-            placeholder="Location"
-          />
-        </template>
-      </Column>
-      <Column
-        :sortable="true"
-        field="status"
-        header="Status"
-        filterMatchMode="contains"
-      >
-        <template #body="slotProps">
-          <span class="p-column-title">Status</span>
-          <span>{{ slotProps.data.status }}</span>
-        </template>
-        <template #filter>
-          <InputText
-            type="text"
-            v-model="filters['status']"
-            class="p-column-filter"
-            placeholder="Status"
-          />
-        </template>
-      </Column>
-      <Column :exportable="false">
-        <template #body="slotProps">
-          <Button
-            label="Update"
-            icon="pi pi-pencil"
-            class="p-button-rounded p-button-text p-mr-2"
-            @click="
-              $router.push({
-                name: 'UpdateBooking',
-                params: { id: slotProps.data.id }
-              })
-            "
-          />
-          <Button
-            label="Report"
-            icon="pi pi-download"
-            class="p-button-rounded p-button-secondary p-button-text"
-          />
-        </template>
-      </Column>
-    </DataTable>
-  </div>
+      <template #filter>
+        <InputText
+          type="text"
+          v-model="filters['name']"
+          class="p-column-filter"
+          placeholder="Name"
+        />
+      </template>
+    </Column>
+    <Column
+      :sortable="true"
+      field="datetime"
+      header="Date"
+      filterMatchMode="custom"
+      :filterFunction="filterDate"
+    >
+      <template #body="slotProps">
+        <span class="p-column-title">Date/Time</span>
+        {{ slotProps.data.datetime }}
+      </template>
+      <template #filter>
+        <Calendar
+          v-model="filters['datetime']"
+          dateFormat="yy-mm-dd"
+          class="p-column-filter"
+          placeholder="Date"
+        />
+      </template>
+    </Column>
+    <Column
+      :sortable="true"
+      field="amount"
+      header="Number of Testees"
+      filterMatchMode="contains"
+    >
+      <template #body="slotProps">
+        <span class="p-column-title">Number of Testees</span>
+        <span>{{ parseInt(slotProps.data.amount) }}</span>
+      </template>
+      <template #filter>
+        <InputText
+          type="text"
+          v-model="filters['amount']"
+          class="p-column-filter"
+          placeholder="Testees"
+        />
+      </template>
+    </Column>
+    <Column
+      :sortable="true"
+      field="location.name"
+      header="Location"
+      filterMatchMode="contains"
+    >
+      <template #body="slotProps">
+        <span class="p-column-title">Location</span>
+        <span>{{ slotProps.data.location.name }}</span>
+      </template>
+      <template #filter>
+        <InputText
+          type="text"
+          v-model="filters['location.name']"
+          class="p-column-filter"
+          placeholder="Location"
+        />
+      </template>
+    </Column>
+    <Column
+      :sortable="true"
+      field="status"
+      header="Status"
+      filterMatchMode="contains"
+    >
+      <template #body="slotProps">
+        <span class="p-column-title">Status</span>
+        <span>{{ slotProps.data.status }}</span>
+      </template>
+      <template #filter>
+        <InputText
+          type="text"
+          v-model="filters['status']"
+          class="p-column-filter"
+          placeholder="Status"
+        />
+      </template>
+    </Column>
+    <Column :exportable="false">
+      <template #body="slotProps">
+        <Button
+          label="Update"
+          icon="pi pi-pencil"
+          class="p-button-rounded p-button-text p-mr-2"
+          @click="
+            $router.push({
+              name: 'UpdateBooking',
+              params: { id: slotProps.data.id }
+            })
+          "
+        />
+        <Button
+          label="Report"
+          icon="pi pi-download"
+          class="p-button-rounded p-button-secondary p-button-text"
+        />
+        <Button
+          label="Delete"
+          icon="pi pi-times"
+          class="p-button-rounded p-button-danger p-button-text"
+          @click="$emit('delete', slotProps.data.id)"
+        />
+      </template>
+    </Column>
+  </DataTable>
 </template>
 
 <script>
+import { ref, computed } from 'vue';
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
 import Button from 'primevue/button';
 import InputText from 'primevue/inputtext';
 import Calendar from 'primevue/calendar';
+import firebase from 'firebase/app';
 export default {
   name: 'BookingsTable',
   components: { DataTable, Column, Button, InputText, Calendar },
@@ -163,18 +166,21 @@ export default {
       default: new Array()
     }
   },
-  data() {
-    return {
-      filters: {}
-    };
-  },
-  computed: {
-    loading() {
-      return this.data.length ? false : true;
-    }
-  },
-  methods: {
-    filterDate(value, filter) {
+  setup(props) {
+    const filters = ref({});
+    const loading = computed(() => {
+      return props.data.length ? false : true;
+    });
+    const dataFormatted = computed(() => {
+      return props.data.map((row) => {
+        if (row.datetime instanceof firebase.firestore.Timestamp)
+          row.datetime = formatDate(row.datetime.toDate(), true);
+
+        return row;
+      });
+    });
+
+    function filterDate(value, filter) {
       if (
         filter === undefined ||
         filter === null ||
@@ -187,9 +193,10 @@ export default {
         return false;
       }
 
-      return value === this.formatDate(filter);
-    },
-    formatDate(date) {
+      return value === formatDate(filter);
+    }
+
+    function formatDate(date, string = false) {
       if (date instanceof Date) {
         let month = date.getMonth() + 1;
         let day = date.getDate();
@@ -202,13 +209,31 @@ export default {
           day = '0' + day;
         }
 
+        if (string) {
+          var options = {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit'
+          };
+          return date.toLocaleDateString('en-CA', options);
+        }
         return date.getFullYear() + '-' + month + '-' + day;
       }
       return date;
-    },
-    exportCSV() {
+    }
+
+    function exportCSV() {
       this.$refs.dt.exportCSV();
     }
+
+    return {
+      filters,
+      loading,
+      dataFormatted,
+      filterDate,
+      formatDate,
+      exportCSV
+    };
   }
 };
 </script>
--- src/views/RequestedBookings.vue
+++ src/views/RequestedBookings.vue
@@ -1,29 +1,84 @@
 <template>
   <h1>Requested Bookings</h1>
-  <bookings-table :data="bookings" />
+  <bookings-table :data="bookings" @delete="deleteBooking" />
+  <Dialog
+    header="Confirmation"
+    v-model:visible="display"
+    :style="{ width: '350px' }"
+    :modal="true"
+  >
+    <div class="confirmation-content">
+      <i class="pi pi-exclamation-triangle p-mr-3" style="font-size: 2rem" />
+      <span>Are you sure you want to delete the booking?</span>
+    </div>
+    <template #footer>
+      <Button
+        label="No"
+        icon="pi pi-times"
+        @click="display = false"
+        class="p-button-text"
+      />
+      <Button
+        label="Yes"
+        icon="pi pi-check"
+        @click="confirm"
+        class="p-button-text"
+        autofocus
+        :disabled="disabled"
+      />
+    </template>
+  </Dialog>
 </template>
 
 <script>
+import { ref } from 'vue';
 import BookingsTable from '@/components/BookingsTable.vue';
+import Dialog from 'primevue/dialog';
+import Button from 'primevue/button';
 import { db } from '@/firebaseConfig.js';
 export default {
   name: 'RequestedBookings',
-  components: { BookingsTable },
-  data() {
-    return {
-      bookings: []
-    };
-  },
-  created() {
-    db.collection('bookings')
-      .get()
-      .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          const booking = doc.data();
-          booking.id = doc.id;
-          this.bookings.push(booking);
+  components: { BookingsTable, Dialog, Button },
+  setup() {
+    const bookings = ref([]);
+    const display = ref(false);
+    const bookingID = ref(null);
+
+    getBookings();
+
+    function getBookings() {
+      db.collection('bookings')
+        .get()
+        .then((querySnapshot) => {
+          bookings.value = [];
+          querySnapshot.forEach((doc) => {
+            const booking = doc.data();
+            booking.id = doc.id;
+            bookings.value.push(booking);
+          });
         });
-      });
+    }
+
+    function deleteBooking(id) {
+      display.value = true;
+      bookingID.value = id;
+    }
+
+    function confirm() {
+      db.collection('bookings')
+        .doc(bookingID.value)
+        .delete()
+        .then(() => {
+          getBookings();
+          display.value = false;
+          console.log('Document successfully deleted: ', bookingID.value);
+        })
+        .catch((error) => {
+          console.error('Error removing document: ', error);
+        });
+    }
+
+    return { bookings, display, deleteBooking, confirm };
   }
 };
 </script>
