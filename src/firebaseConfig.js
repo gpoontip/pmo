3940a2dce460336ec65ddc591b28acd2e9323455
Gary Poon Tip <gpoontip@fieldsafesolutions.com>
2020-11-22 23:06:01 -0700

    test firebase env

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,16 +1,18 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: process.env.VUE_APP_APIKEY,
-  authDomain: process.env.VUE_APP_AUTHDOMAIN,
-  databaseURL: process.env.VUE_APP_DATABASEURL,
-  projectId: process.env.VUE_APP_PROJECTID,
-  storageBucket: process.env.VUE_APP_STORAGEBUCKET,
-  messagingSenderId: process.env.VUE_APP_MESSAGINGSENDERID,
-  appId: process.env.VUE_APP_APPID,
-  measurementId: process.env.VUE_APP_MEASUREMENTID
-};
+// const firebaseConfig = {
+//   apiKey: process.env.VUE_APP_APIKEY,
+//   authDomain: process.env.VUE_APP_AUTHDOMAIN,
+//   databaseURL: process.env.VUE_APP_DATABASEURL,
+//   projectId: process.env.VUE_APP_PROJECTID,
+//   storageBucket: process.env.VUE_APP_STORAGEBUCKET,
+//   messagingSenderId: process.env.VUE_APP_MESSAGINGSENDERID,
+//   appId: process.env.VUE_APP_APPID,
+//   measurementId: process.env.VUE_APP_MEASUREMENTID
+// };
+
+const firebaseConfig = process.env.FIREBASE_CONFIG;
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
